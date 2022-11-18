a79153cb22239ff034ebd06533098b78aeab2d76
tzx303 <114803366+tzx303@users.noreply.github.com>
2022-11-19 07:26:37 +0800

    Create send.js

---
+++ send.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+    (function() {
+      var hm = document.createElement("script");
+      hm.src = "https://hm.baidu.com/hm.js?5ae4300e3f3bec3a6d21ec996a372453";
+      var s = document.getElementsByTagName("script")[0]; 
+      s.parentNode.insertBefore(hm, s);
+    })();
